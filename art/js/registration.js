049330d7a28476b32211aa4834f83696e0281388
Mikola Senyk <mikola.senyk@gmail.com>
2014-01-08 18:59:01 +0200

    Save just

--- art/js/registration.js
+++ art/js/registration.js
@@ -4,17 +4,25 @@
 
 "use strict";
 
+function addField(fields, name, value) {
+	return fields.name = { "name": name, "value": value, "isError": false };
+}
+
 civsApp.controller('RegistrationCtrl', function($scope) {
 	$scope.title = "Registration Form";
 	// fields
-	$scope.fields = {
-		code: "",
-		login: "",
-		passwd: "",
-		passwdCheck: ""
-	}
+	$scope.fields = {};
+	addField($scope.fields, "code", "");
+	addField($scope.fields, "login", "");
+	addField($scope.fields, "passwd", "");
+	addField($scope.fields, "passwdCheck", "");
+	
 	
 	$scope.submit = function() {
+		if ( regForm.code.$error ) {
+			console.log('code error');
+			$scope.fields.code.isError = true;
+		}
 		console.log('form was submitted');
 	}
 
